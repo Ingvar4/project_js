81912acace2a45161b7bbf2937efc26da24af303
Ingvar <jamessoer91@gmail.com>
2025-03-18 13:59:36 +0300

    examples page

--- scripts/script-other1.js
+++ scripts/script-other1.js
@@ -1,2 +1,21 @@
 
+// function question() {
+//   let age = +prompt('Введите ваш возраст');
 
+//   if (isNaN(age)) {
+//     alert('Вы ввели не число. Пожалуйста введите число');
+//     question();
+//   } else {
+//     message(age);
+//   }
+// }
+
+// question();
+
+// function message(age) {
+//   if (age >= 16) {
+//     alert('Доступ разрешен');
+//   } else {
+//     alert('Доступ ограничен');
+//   }
+// }
