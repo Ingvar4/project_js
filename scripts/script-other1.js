f5d8e5d3fe69cdf3c6a18d1a58c0d09259ad7f5e
Ingvar <jamessoer91@gmail.com>
2025-03-21 13:59:32 +0300

    ++

---
+++ scripts/script-other1.js
@@ -0,0 +1,21 @@
+// let data = ['ПН', 'ВТ', 'СР', 'ЧТ'];
+// let index = 0;
+
+// while (index < data.length) {
+//   console.log('Элемент ' + data[index]);
+//   index++;
+// }
+
+
+
+// let numbers = [5, 8, 10, 10, 5, 7, 19];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//   if (numbers[i] > 10) {
+//     console.log('Найдено число больше 10: ' + numbers[i]);
+//     break;
+//   }
+//   i++;
+// }
