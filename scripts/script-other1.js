50070c6d4047a12eb0d33ea8a0cad534ec53ffaf
Ingvar <jamessoer91@gmail.com>
2025-05-03 13:27:45 +0300

    +examples

--- scripts/script-other1.js
+++
@@ -1,7 +0,0 @@
-const listItems = document.getElementById('list');
-
-const items = ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5',];
-
-const htmlContent = items.map((item) => `<li>${item}</li>`).join('');
-
-listItems.innerHTML = htmlContent;
